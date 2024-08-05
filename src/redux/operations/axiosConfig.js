9e4781d010970f4a5ee67295b50ed86dabb341d4
Homrts620 <hom620@gmail.com>
2024-08-06 01:00:42 +0200

    Update axiosConfig.js

--- src/redux/operations/axiosConfig.js
+++ src/redux/operations/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global/mockapi.io';
 
 export default axios;
