e1fe2a44e8a6554fb18b7249e256d30cf469fa0d
Homrts620 <hom620@gmail.com>
2025-02-26 18:30:15 +0100

    Update axiosConfig.js

--- src/redux/operations/axiosConfig.js
+++ src/redux/operations/axiosConfig.js
@@ -1,12 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const swagger = axios.create({
-  baseURL: "https://connections-api.goit.global/",
-});
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
-export const setToken = (token) => {
-  swagger.defaults.headers.common.Authorization = Bearer ${token};
-};
-export const clearToken = () => {
-  swagger.defaults.headers.common.Authorization = ``;
-};
+export default axios;
